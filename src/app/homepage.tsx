08b9a6ddac0099340560b6bfc39ccea7c9638173
Ismail Muhammed Hammed <66277089+IsmailMuhammed2019@users.noreply.github.com>
2024-12-16 17:23:25 +0100

    Update homepage.tsx with clean code

--- src/app/homepage.tsx
+++ src/app/homepage.tsx
@@ -5,13 +5,7 @@ import './homepage.css'
 
 function homepage() {
   return (
-    <div className="coming-soon-container">
-      <div className="coming-soon">
-        <FontAwesomeIcon icon={faHourglassHalf} className="spinning-icon" size="3x" />
-        <h2>Coming Soon</h2>
-        <p>We are working hard to bring you something amazing. Stay tuned!</p>
-      </div>
-    </div>
+    <h1>.</h1>
   )
 }
 
