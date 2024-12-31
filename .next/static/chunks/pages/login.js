81e8e3d3552710135c94f6816b3826164d63bf5b
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2024-12-31 12:01:36 +0100

    update login page to be responsive

---
+++ .next/static/chunks/pages/login.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/login", [
+  "static/chunks/[root of the server]__4a4b6d._.js",
+  "static/chunks/node_modules_next_408a95._.js",
+  "static/chunks/node_modules_react-dom_82bb97._.js",
+  "static/chunks/node_modules_1b7400._.js",
+  "static/chunks/pages_login_5771e1._.js",
+  "static/chunks/pages_login_1bb9ec._.js"
+])
--- pages/login.js
+++ pages/login.js
@@ -4,7 +4,7 @@ import Footer from './components/footer'
 import Image from 'next/image'
 import Link from 'next/link'
 
-function login() {
+function Login() {
   return (
     <>
     <Header />
@@ -12,7 +12,7 @@ function login() {
       {/* Center Section */}
       <section id="center" className="center_log">
         <div className="center_om bg_backn1">
-          <div className="container-xl">
+          <div className="container-fluid">
             <div className="row center_o1 text-center">
               <div className="col-md-12">
                 <h1 className="text-white">Login</h1>
@@ -28,16 +28,16 @@ function login() {
 
       {/* Login Section */}
       <section id="login" className="p_3">
-        <div className="container-xl">
+        <div className="container-fluid">
           <div className="row">
             <div className="col-md-12">
-              <div className="login_1 p-4 mx-auto">
-                <h3 className="col_red">Login</h3>
-                <p className="center_sm">Login with your account</p>
+              <div className="login_1 p-4 mx-auto" style={{ borderRadius: '10px', backgroundColor: 'white' }}>
+                <h3 className="col_red text-center">Login</h3>
+                <p className="center_sm text-center">Login with your account</p>
                 <h6 className="mt-4">Email Address</h6>
-                <input type="email" className="form-control" placeholder="Your Email" />
+                <input type="email" className="form-control" placeholder="Your Email" style={{ borderRadius: '10px' }} />
                 <h6 className="mt-4">Password</h6>
-                <input type="password" className="form-control" placeholder="Your Password" />
+                <input type="password" className="form-control" placeholder="Your Password" style={{ borderRadius: '10px' }} />
                 <div className="d-flex justify-content-between mt-4">
                   <div className="form-check">
                     <input className="form-check-input" type="checkbox" value="" id="remember" />
@@ -47,10 +47,10 @@ function login() {
                   </div>
                   <Link className="col_red" href="#">Forgot Password?</Link>
                 </div>
-                <h6 className="mt-4 mb-0 center_sm">
-                  <Link className="button" href="#">Login <i className="fa fa-sign-in ms-1"></i></Link>
+                <h6 className="mt-4 mb-0 center_sm text-center">
+                  <Link className="button" href="#" style={{ borderRadius: '10px' }}>Login <i className="fa fa-sign-in ms-1"></i></Link>
                 </h6>
-                <p className="mt-4 mb-0">Don't have an account? <Link className="col_red" href="register.html">Register</Link></p>
+                <p className="mt-4 mb-0 text-center">Don't have an account? <Link className="col_red" href="register.html">Register</Link></p>
               </div>
             </div>
           </div>
@@ -63,4 +63,4 @@ function login() {
   )
 }
 
-export default login
+export default Login
